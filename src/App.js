747039e816fd9f20753a4b36810124da009cdd89
Joseph Alvini <josephalvini13@gmail.com>
2019-09-14 23:15:28 -0400

    Moved all code from App.js to Wrapper.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./views/Home";
-import Contact from "./views/Contact";
-import About from "./views/About";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import './App.css';
-
-const Code = () => (
-    <div>
-        Code
-    </div>
-);
+import './App.sass';
+import Wrapper from "./views/layout/Wrapper";
 
 class App extends Component {
   render() {
     return(
-        <Router>
-            <div>
-                <Header />
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/contact" component={Contact} />
-                <Route path="/code" component={Code} />
-                <Footer />
-            </div>
-        </Router>
+        <Wrapper />
     )
   }
 }
